275ff657fd27552ffa5d1f00347193a36e3f4d0b
shuraosipov <shuraosipov@github.com>
2016-12-18 18:46:28 +0300

    changed

--- win2012_r2_template/scripts/packer-shutdown.bat
+++ win2012_r2_template/scripts/packer-shutdown.bat
@@ -1,2 +1,1 @@
-netsh advfirewall firewall set rule name="WinRM-HTTP" new action=block
-C:/windows/system32/sysprep/sysprep.exe /generalize /oobe /unattend:C:/Windows/packer/unattended.xml /quiet /shutdown
+C:/windows/system32/sysprep/sysprep.exe /generalize /oobe /quiet /shutdown
--- win2012_r2_template/scripts/prepare-environment.ps1
+++ win2012_r2_template/scripts/prepare-environment.ps1
@@ -1,6 +1,7 @@
 # Create Powershell profile 
 # https://blogs.technet.microsoft.com/heyscriptingguy/2012/05/21/understanding-the-six-powershell-profiles/
 New-Item -Path $profile -Type file -Force
+echo "#Dummy content to avoid error Not setting tab completion: File is Authenticode signed at 'profile'" > $profile
 
 # Installing Chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
